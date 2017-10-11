7de04d6531e659637afc08ecc082d0df023c8453
merisahakyan <sahakyan_m@bk.ru>
2017-10-11 14:20:59 +0400

    get Tenant from request

--- App/App/Controllers/MultiTenantMvcController.cs
+++ App/App/Controllers/MultiTenantMvcController.cs
@@ -1,20 +1,28 @@
-﻿using System;
+﻿using App.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 using System.Web.Mvc;
+using System.Web;
 
 namespace App.Controllers
 {
-    class MultiTenantMvcController:Controller
+    public class MultiTenantMvcController:Controller
     {
-        public TenantController Tenant
+        public Tenant Tenant
         {
             get
             {
                 object multiTenant;
-                if(!Request.GetOwinContext().)
+                if(!Request.GetOwinContext().Environment.TryGetValue("MultiTenant",out multiTenant))
+                {
+                    throw new ApplicationException("could not find Tenant");
+                }
+                return (Tenant)multiTenant;
             }
         }
     }
--- App/App/Controllers/SpeakerController.cs
+++ App/App/Controllers/SpeakerController.cs
@@ -7,11 +7,11 @@ using System.Web.Mvc;
 
 namespace App.Controllers
 {
-    public class SpeakerController : Controller
+    public class SpeakerController : MultiTenantMvcController
     {
         public ActionResult Index()
         {
-            return View();
+            return View("Index",Tenant);
         }
     }
 }
